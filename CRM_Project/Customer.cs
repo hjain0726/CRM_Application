1552aedc1d175306e94ee9074cc3ba1be9c72b16
Harsh Jain <31067957+hjain0726@users.noreply.github.com>
2020-02-06 16:32:42 +0530

    Update Customer.cs

--- CRM_Project/Customer.cs
+++ CRM_Project/Customer.cs
@@ -12,20 +12,5 @@ namespace CRM_Project
         public string customer_pwd { get; set; }
         public string customer_address { get; set; }
         public string custome_phone { get; set; }
-
-        /*public string get_customer_id()
-        {
-            return this.customer_id;
-        }*/
-
-       /* public void setCustomer(string _id,string name, string email,string pwd,string address, string phone)
-        {
-            this.customer_id = _id;
-            this.customer_name = name;
-            this.customer_email = email;
-            this.customer_pwd = pwd;
-            this.customer_address = address;
-            this.custome_phone = phone;
-        }*/
     }
 }
