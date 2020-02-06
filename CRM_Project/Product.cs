7c8929c77555699c57254fb29942541b4240c957
Harsh Jain <31067957+hjain0726@users.noreply.github.com>
2020-02-06 16:34:10 +0530

    Update Product.cs

--- CRM_Project/Product.cs
+++ CRM_Project/Product.cs
@@ -11,23 +11,5 @@ namespace CRM_Project
         public string product_category { set; get; }
         public double product_price { set; get; }
         public int product_stock { set; get; }
-
-       /* public string get_product_id()
-        {
-            return this.product_id;
-        }
-        public int get_price()
-        {
-            return this.product_price;
-        }
-
-        public void setProduct(string id,string name,string category,int price,int stock)
-        {
-            this.product_id = id;
-            this.product_name = name;
-            this.product_category = category;
-            this.product_price = price;
-            this.product_stock = stock;
-        }*/
     }
 }
